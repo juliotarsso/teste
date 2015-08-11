de9dd4d6ebad43bc198319a0f0761026bfb21dc1
juliotarsso <julio_molina18@hotmail.com>
2015-08-11 20:27:32 -0300

    criacao da classe exemplo

---
+++ src/teste/Exemplo.java
@@ -0,0 +1,22 @@
+package teste;
+
+public class Exemplo {
+	
+	private int id;
+	private String nome;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+
+}
